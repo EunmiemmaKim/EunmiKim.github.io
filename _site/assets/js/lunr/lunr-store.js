65c61c598572e09e89b0bf297d57d1c108023159
EunmiemmaKim <eunmiemma.gmail.com>
2021-07-28 11:17:53 +0900

    4

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -41,73 +41,7 @@ var store = [{
         "url": "/portfolio/index.html",
         "teaser": null
       },{
-        "title": "Anomaly Detection in Dynamic Networks",
-        "excerpt":"“Data analysis” is a hugely popular thing these days, for obvious reasons. When most people think of “data,” they think of a table where the columns are variables and the rows are observations…   If you are not automatically redirected, please click here     ","categories": ["posts"],
-        "tags": [],
-        "url": "/posts/dynamic",
-        "teaser": null
-      },{
-        "title": "Building a Personal Site with Jekyll & Minimal Mistakes",
-        "excerpt":"I learned a lot while building this website; I hope to share it so that it might be helpful for anyone trying to do the same. I’m sure you’ll notice that I’m far from an expert in the subjects we’re going to explore here; this is my first foray into...","categories": ["posts"],
-        "tags": [],
-        "url": "/posts/website",
-        "teaser": null
-      },{
-        "title": "The Meaning of Entropy",
-        "excerpt":"Entropy is a word that we see a lot in various forms. It’s classical use comes from thermodynamics: e.g. “the entropy in the universe is always increasing.” With the recent boom in statistics and machine learning, the word has also seen a surge in use in information-theoretic contexts: e.g. “minimize...","categories": ["posts"],
-        "tags": [],
-        "url": "/posts/entropy",
-        "teaser": null
-      },{
-        "title": "Algorithmic Musical Genre Classification",
-        "excerpt":"If you are not automatically redirected, please click here     ","categories": ["posts"],
-        "tags": [],
-        "url": "/posts/genre",
-        "teaser": null
-      },{
-        "title": "Inverse Transform Sampling in Python",
-        "excerpt":"When doing data work, we often need to sample random variables. This is easy to do if one wishes to sample from a Gaussian, or a uniform random variable, or a variety of other common distributions, but what if we want to sample from an arbitrary distribution? There is no...","categories": ["posts"],
-        "tags": [],
-        "url": "/posts/sampling",
-        "teaser": null
-      },{
-        "title": "Types as Propositions",
-        "excerpt":"Some of the most meaningful mathematical realizations that I’ve had have been unexpected connections between two topics; that is, realizing that two concepts that first appeared quite distinct are in fact one and the same. In our first linear algebra courses, we learn that manipulations of matrices is, in fact,...","categories": ["posts"],
-        "tags": [],
-        "url": "/posts/types",
-        "teaser": null
-      },{
-        "title": "New Paper: Metrics For Graph Comparison",
-        "excerpt":"I just put a new paper up on the arXiv, and so I thought I would share it here. This was the final paper I wrote for my Ph.D., and it’s the one I’m most proud of. The paper is called “Metrics for Graph Comparison: a Practitioner’s Guide.” The Basic...","categories": ["posts"],
-        "tags": [],
-        "url": "/posts/metrics-paper",
-        "teaser": null
-      },{
-        "title": "DS Interview Study Guide Part I: Statistics",
-        "excerpt":"As I have gone through a couple rounds of interviews for data scientist positions, I’ve been compiling notes on what I consider to be the essential areas of knowledge. I want to make these notes available to the general public; although there are many blog posts out there that are...","categories": ["posts"],
-        "tags": [],
-        "url": "/posts/stats",
-        "teaser": null
-      },{
-        "title": "DS Interview Study Guide Part II: Software Engineering",
-        "excerpt":"This post continues my series on data science interviews. One of the major difficulty of doing data science interviews is that you must show expertise in a wide variety of skills. In particular, I see four key subject areas that you might be asked about during an interview: Statistics Software...","categories": ["posts"],
-        "tags": [],
-        "url": "/posts/engineering",
-        "teaser": null
-      },{
-        "title": "Your p-values Are Bogus",
-        "excerpt":"People often use a Gaussian to approximate distributions of sample means. This is generally justified by the central limit theorem, which states that the sample mean of an independent and identically distributed sequence of random variables converges to a normal random variable in distribution.1 In hypothesis testing, we might use...","categories": ["posts"],
-        "tags": [],
-        "url": "/posts/bogus",
-        "teaser": null
-      },{
-        "title": "Blogging in Org Mode",
-        "excerpt":"I recently transitioned from writing my posts directly in markdown to writing them in org mode, a document authoring system built in GNU Emacs. I learned a lot in the process, and also built a new org exporter in the process, ox-jekyll-lite.1 Org Mode and the Meaning of Life What...","categories": ["post"],
-        "tags": [],
-        "url": "/post/blogging-in-org",
-        "teaser": null
-      },{
-        "title": "NetComp: Network Comparison in Python",
+        "title": "Bank data analysis in Python",
         "excerpt":"If you are not automatically redirected, please click here     ","categories": ["posts"],
         "tags": [],
         "url": "/posts/churn",
